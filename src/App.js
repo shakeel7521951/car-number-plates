422f16cfb9f6efdc69a1f6eb00116d65bced1856
Muhammad Shakeel <shakeel7521951@gmail.com>
2024-11-07 16:57:15 +0500

    first comment

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './Pages/Home';
+import Normal from './Pages/Normal';
+import Silver from './Pages/Silver';
+import Golden from './Pages/Golden';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+import Footer from './components/Footer';
+
+// Import your components
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/normal' element={<Normal />} />
+          <Route path='/silver' element={<Silver />} />
+          <Route path='/golden' element={<Golden />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Golden.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Golden = () => {
+  return (
+    <div>Golden</div>
+  )
+}
+
+export default Golden
