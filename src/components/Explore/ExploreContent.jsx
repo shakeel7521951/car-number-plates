f97a429b50b83a8e7384f70081fb00303679c82f
Ahmad Mamoon <ahmadeveloper077@gmail.com>
2024-11-08 18:07:26 +0500

    Set the  Explore section and data

--- src/components/Explore/ExploreContent.jsx
+++ src/components/Explore/ExploreContent.jsx
@@ -1,18 +1,19 @@
 import ExploreCard from './ExploreCard';
+import { data } from '../../StaticData/data';
 
 const ExploreContent = () => {
   return (
     <main>
       <h1 className='text-2xl font-bold text-white my-4'>Explore More Now </h1>
-      <p className='text-white my-2'>Plates Numbers: 2250</p>
+      <p className='text-white my-2'>Plates Numbers: {data.length}</p>
 
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-between gap-4 max-w-[1500px] mx-auto'>
-        {' '}
-        <ExploreCard />
-        <ExploreCard />
-        <ExploreCard />
+        {data.map((plate) => (
+          <ExploreCard {...plate} />
+        ))}
       </div>
     </main>
   );
 };
+
 export default ExploreContent;
