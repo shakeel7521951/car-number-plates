a3456cda4efa57e45bf5b6ae884f0e73c8eb51e0
Ahmad Mamoon <ahmadeveloper077@gmail.com>
2024-11-08 16:27:17 +0500

    Merge all the conflicts

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,30 +1,19 @@
-<<<<<<< HEAD
-import React from 'react'
-import Hero from '../components/Hero'
-import Today from '../components/Today'
-const Home = () => {
-  return (
-    <div>
-      Home
-      <Hero/>
-      <Today/>
-    </div>
-
-  )
-}
-=======
 import React from 'react';
+import Hero from '../components/Hero';
+import Today from '../components/Today';
 import Summary from '../components/Home/Summary';
 import Reviews from '../components/Home/Reviews';
 
 const Home = () => {
   return (
     <div>
+      Home
+      <Hero />
+      <Today />
       <Summary />
       <Reviews />
     </div>
   );
 };
->>>>>>> f24cd2d08629e130cfdb79e5c5b3061192f68bc0
 
 export default Home;
