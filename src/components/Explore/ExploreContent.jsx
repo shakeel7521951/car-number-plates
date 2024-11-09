153826281aa2bd35b7ccfad7422592d174721fab
Ahmad Mamoon <ahmadeveloper077@gmail.com>
2024-11-09 11:49:22 +0500

    Updated Explore Content

--- src/components/Explore/ExploreContent.jsx
+++ src/components/Explore/ExploreContent.jsx
@@ -1,15 +1,20 @@
 import ExploreCard from './ExploreCard';
 import { data } from '../../StaticData/data';
+import { useEffect } from 'react';
 
 const ExploreContent = () => {
-  
+  useEffect(() => {
+    window.scroll(0, 0);
+  }, []);
+
   return (
     <main>
       <h1 className='text-2xl font-bold text-white my-4'>Explore More Now </h1>
-      <p className='text-white my-2'>Plates Numbers: {data?.length}</p>
+      <p className='text-white my-2'>Plates Numbers: {data.length}</p>
+
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-between gap-4 max-w-[1500px] mx-auto'>
-        {data?.map((plate,index) => (
-          <ExploreCard {...plate} key={index} />
+        {data.map((plate) => (
+          <ExploreCard {...plate} />
         ))}
       </div>
     </main>
